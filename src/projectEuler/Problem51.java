bbc1dfb07a0943c40814f1afc93d5a9eaa1c68b3
Kyle Chamberlin <KyleChamberlin@project20million.org>
2012-07-13 16:02:55 -0400

    Cleaned up solution to Problem 51
    Finally got it...

--- src/projectEuler/Problem51.java
+++ src/projectEuler/Problem51.java
@@ -9,29 +9,18 @@ public class Problem51 {
 	int primeIndex = 1;
 	int prime = 2;
 	int limit = 100000000;
-	int listLimit = 0;
-	int sieveBound = (limit - 1) /2;
-	double crosslimit = (Math.sqrt(limit) - 1) / 2;
 	BitSet primeList;
 
 	public Problem51(){
 		primeSieve(limit);
-		System.out.println("finished Sieving");
-		int most = 0;
-		int howmanytimes = 1;
-		int max = primeList.cardinality();
-		while(most < 8 && howmanytimes < max){
+		while(true){
 			nextPrime();
-			int temp = numberInFamily2(prime);
-			int temp2 = numberInFamily3(prime);
-			temp = (temp>temp2?temp:temp2);
-			if(temp > most){
-				most = temp;
+			int temp = numberInFamily(prime);
+			if(temp==8){
 				answer=prime;
+				break;
 			}
-			howmanytimes++;
 		}
-		//answer=prime;
 	}
 		
 	public int getAnswer(){
@@ -52,23 +41,16 @@ public class Problem51 {
 	            int inc = num << 1;
 	            for(int factor = num * num; factor < mLimit; factor += inc) 
 	            { 
-	                //if( ((factor) & 1) == 1) 
-	                //{ 
 	                    primeList.clear(factor >> 1); 
-	                //} 
 	            } 
 	        } 
 	    } 
-	    listLimit = primeList.size();
 	} 
 	
 	private void nextPrime() {
 		primeIndex++;
 		primeIndex = primeList.nextSetBit(primeIndex);
 		prime = primeIndex*2 +1;
-		
-		//System.out.println(prime);
-		
 	}
 
 	public boolean isPrime(int num) 
@@ -83,38 +65,7 @@ public class Problem51 {
 	    return false;
 	}
 	
-	private int numberInFamily2(int pN){
-		int maxCount = 0;
-		int count = 0;
-		for(int p1 = 10; p1 < pN; p1*=10){
-			int digit1 = (pN%(p1*10)-pN%(p1)) / p1;
-			for(int p2 = 10;p2 < p1; p2*=10){
-				count = 0;				
-				int digit2 = (pN%(p2*10)-pN%(p2)) / p2;
-				int start = (digit1>digit2 ? digit1 : digit2);
-				int[] arrayOfChecks = new int[]{0,0,0,0,0,0,0,0,0,0};
-				for(int num = 0; digit1 == digit2 && digit2 < 3L && num + start < 10 ;num++){
-					int numToAdd = (num*p1) + ((num)*p2);
-					int check = pN+numToAdd;
-					arrayOfChecks[num] = check;
-					if(check != pN && isPrime(check)){
-						//System.out.print(""+ check + " ");
-						count++;
-					}
-				}
-				if(count > maxCount){
-					maxCount = count;
-					if(maxCount > 5){
-						System.out.println("" + pN + " count: " + count);
-					}
-					//System.out.println(arrayOfChecks.toString());
-				}
-				//System.out.println(" -> "+count);
-			}
-		}
-		return maxCount + 1;
-	}	
-	private int numberInFamily3(int pN){
+	private int numberInFamily(int pN){
 		int maxCount = 0;
 		int count = 0;
 		for(int p1 = 10; p1 < pN; p1*=10){
@@ -131,19 +82,13 @@ public class Problem51 {
 						int check = pN+numToAdd;
 						arrayOfChecks[num] = check;
 						if(check != pN && isPrime(check)){
-							//System.out.print(""+ check + " ");
 							count++;
 						}
 					}
 					if(count > maxCount){
 						maxCount = count;
-						if(maxCount > 5){
-							System.out.println("" + pN + " count: " + count);
-						}
-						//System.out.println(arrayOfChecks.toString());
 					}
 				}
-				//System.out.println(" -> "+count);
 			}
 		}
 		return maxCount + 1;
