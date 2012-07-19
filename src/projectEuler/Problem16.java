f4918aacb2178e1d161a90d4c889fc47ee9819fe
mthguy <mthguy@DevLaptop>
2012-07-19 16:54:31 -0400

    Solution to Problem 16

--- src/projectEuler/Problem16.java
+++ src/projectEuler/Problem16.java
@@ -1,36 +1,41 @@
 package projectEuler;
 
-import java.math.BigInteger;
-
 public class Problem16 {
-
+	
+	int[] a;
 	int answer = 0;
-	/**
-	 * 
-	 * @param args
-	 */
+
 	public Problem16(){
-		BigInteger num = new BigInteger("2");
-		
-		for(int i=1;i<1000;i++){
-			num = num.multiply(new BigInteger("2"));
-		}
-		
-		String digits = num.toString();
-		answer = sumDigits(digits);		
+		twoPower(1000);
+		answer = sumDigits(a);		
 	}
 	
 	public int getAnswer(){
 		return answer;
 	}
 	
-	private int sumDigits(String pDigits){
-		int sum = 0;
-		
-		for(int i = 0 ; i < pDigits.length(); i++){
-			sum += pDigits.charAt(i);
+	public void twoPower(int pN){
+		a = new int[(int) Math.ceil(Math.log10(2)*pN) + 1];
+		java.util.Arrays.fill(a,0);		
+		a[0]=2;
+		for(int n = 1; n < pN; n++){
+			for(int i = 0; i < a.length; i++){
+				a[i] = a[i]*2;
+			}
+			for(int j=0;j<a.length;j++){
+				if(a[j]>=10){
+					a[j+1]+=(a[j]/10);
+					a[j]=a[j]%10;					
+				}
+			}
 		}
-		
+	}
+	
+	private int sumDigits(int[] pDigits){
+		int sum = 0;
+		for(int i : a){
+			sum += i;
+		}
 		return sum;
 	}
 }
