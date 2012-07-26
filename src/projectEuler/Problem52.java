2e40138f8d32671f7dd767a8e345efb31ce90f9f
mthguy <mthguy@DevLaptop.(none)>
2012-07-26 16:09:56 -0400

    lap

--- src/projectEuler/Problem52.java
+++ src/projectEuler/Problem52.java
@@ -20,7 +20,23 @@ public class Problem52 {
 		return val;
 	}
 	
-	private 
+	public int[] bigPower(long pA, long pB){
+		int[] number = new int[(int) Math.ceil(Math.log10(pN)*pn) + 1];
+		java.util.Arrays.fill(number,0);		
+		number[0]=pN;
+		for(int n = 1; n < pn; n++){
+			for(int i = 0; i < number.length; i++){
+				number[i] = number[i]*2;
+			}
+			for(int j=0;j<number.length;j++){
+				if(number[j]>=10){
+					number[j+1]+=(number[j]/10);
+					number[j]=number[j]%10;					
+				}
+			}
+		}
+		return number;
+	}
 
 	public int getAnswer(){
 		return answer;
