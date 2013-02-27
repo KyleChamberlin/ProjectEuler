8c738fe09ff638f488893c87ad06b2dd573c70b6
Kyle Chamberlin <KyleChamberlin@project20million.org>
2013-02-27 07:50:42 -0500

    Add Modular Expentiation to Problem 411
    Unfinished.

--- src/projectEuler/Problem411.java
+++ src/projectEuler/Problem411.java
@@ -42,6 +42,20 @@ public class Problem411 {
 		return answer;
 	}
 	
+	private int modular(int base, int exp, int mod)
+	{
+	    int x = 1;
+	    int power = base % mod;
+
+	    for (int i = 0; i < sizeof(int) * 8; i++) {
+	        int least_sig_bit = 0x00000001 & (exp >> i);
+	        if (least_sig_bit)
+	            x = (x * power) % mod;
+	        power = (power * power) % mod;
+	    }
+
+	    return x;
+	}
 	
 
 }
