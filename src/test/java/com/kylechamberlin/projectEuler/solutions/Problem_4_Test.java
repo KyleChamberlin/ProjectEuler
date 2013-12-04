79defc205350a8a88b551597af1d1ed278544de1
Kyle Chamberlin <KyleChamberlin@project20million.org>
2013-12-04 09:45:07 -0500

    testing ssh pushing to github

---
+++ src/test/java/com/kylechamberlin/projectEuler/solutions/Problem_4_Test.java
@@ -0,0 +1,28 @@
+package com.kylechamberlin.projectEuler.solutions;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class Problem_4_Test {
+
+    private Problem4 problem4;
+
+    @Before
+    public void setUp() {
+        problem4 = new Problem4();
+    }
+
+    @Test
+    public void palindrome9009() {
+            assertTrue(true); // TODO: write actual test.
+    }
+
+    @After
+    public void tearDown() {
+        problem4 = null;
+    }
+}
